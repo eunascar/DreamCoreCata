64f494111f02606d486067dac2b95efdb0090968
Mike <mike2011lp@gmail.com>
2011-03-17 18:27:17 -0500

    Custom: DB Loot Templates For Item Morpher
    Signed-off-by: Mike <mike2011lp@gmail.com>

---
+++ sql/custom/updates/2011_03_17_0_world_creature_loot_template.sql
@@ -0,0 +1,11 @@
+-- Drop Rate for Boss Morpher
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `maxcount`) VALUES
+(150000, 54516, 100, 5),
+(150001, 54516, 100, 5),
+(150002, 54516, 100, 5),
+(150003, 54516, 100, 5);
+-- Drop Rate for NPC Morpher
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `maxcount`) VALUES
+(200002, 54537, 5, 5),
+(200003, 54537, 5, 5),
+(200004, 54537, 5, 5);
